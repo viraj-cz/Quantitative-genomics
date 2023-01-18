c382c1b33cdd0ea9e5de268403505cb6f5a1b5ff
Viraj cz <66521928+viraj-cz@users.noreply.github.com>
2023-01-18 13:25:27 -0800

    Create GWAS.R

---
+++ GWAS.R
@@ -0,0 +1,24 @@
+#Yet to do the computational part
+
+#GWAS - Genome Wide Association Studies
+
+#We try to find the relationship between Phenotypes and Genotypes
+#Phenotypes can be both simply mendellian and influenced by one gene and also be very very abstracted like how rich someone is and is there a genetic correlation to it
+
+#side note: in most cases the genome impacts the phenotype, but sometimes, in very edge cases the reverse can happen too! insane!
+
+#in GWAS we correlate the genome to phenotype using linear regression
+#what we want to do is find out what parts of the genome are affecting our phenotype and not the code of genome itself
+
+
+#y∼βx+e
+
+
+#y represents the phenotypes
+#∼ reads as correlated to
+#β is the effect of a SNP (this is basically the slope -- what we are solving for)
+#x is the SNP/genotype/QTL/Loci
+#e represents the error or effect of the environment
+
+#basicallt the higher the β, the higher the correlation. 1 represents perfect correlation
+
